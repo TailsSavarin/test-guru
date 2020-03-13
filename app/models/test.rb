0c2eab49c205a5912b6a1d4e633e77b6eb170773
TailsSavarin <Alexql7@gmail.com>
2020-03-13 20:44:41 +0300

    lesson5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
 
   def self.sort_tests_by(category_title)
     joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
