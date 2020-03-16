2adc95af3cb2f825d8f9e19a71ad7bb822cffeb6
TailsSavarin <Alexql7@gmail.com>
2020-03-16 22:41:27 +0300

    Add validation methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,9 +5,14 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
+  validates :title, presence: true
+                    uniqueness: { scope: :level }
+
+  validates :level, numericality: { only_integer: true }, greater_than_or_equal_to: 0
+
   scope :easy, -> { where(level: 0..1) }
-  scope :normal, -> { where(level: 2..3) }
-  scope :hard, -> { where(level: 4..Float::INFINITY) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :by_category_title, -> (category_title) { joins(:category).where(categories: { title: category_title })
 
   def self.sort_by(category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :passed_tests
   has_many :author_tests, class_name: 'Test'
 
+  validates :email, presence: true
+
   scope :by_level, -> (level) { where(level: level)
 
   def sort_by(level)
