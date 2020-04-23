24e6dfbcfd91a4f30fd649fcd242beafeb7ed723
TailsSavarin <Alexql7@gmail.com>
2020-04-23 18:18:11 +0300

    lesson tasks and some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
-  helper_method :current_user
+  helper_method :current_user,
+                :logged_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      cookies[:target_url] = request.fullpath
+      redirect_to login_path, alert: 'Please Sign in'
     end
   end
 
@@ -16,7 +18,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def logged_in
+  def logged_in?
     current_user.present?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
 
-  before_action :set_user, :only %i[new, create]
+  skip_before_action :authenticate_user!, only: %i[new create]
 
   def new
-
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to cookies[:target_url] || root_path
     else
+      flash.now[:alert] = 'Please verify your Email and Password'
       render :new
     end
   end
 
-  private
-
-  def set_user
-    @user = User.find_by(email: params[:email])
+  def destroy
+    session[:user_id] = nil
+    flash[:alert] = 'You are Logged out!'
+    redirect_to login_path
   end
 end
