0b565647f4e6b3e4be5401f2e3546e15ea875d3e
TailsSavarin <Alexql7@gmail.com>
2020-05-04 18:16:52 +0300

    fixed views and gist helper

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,8 +2,4 @@ module TestPassagesHelper
   def test_complete_message(test_passage)
     test_passage.success? ? t('.test_pass_success') : t('.test_pass_fail')
   end
-
-  def url
-    "https://gist.github.com/#{github_id}"
-  end
 end
